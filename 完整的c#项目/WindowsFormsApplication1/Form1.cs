5e1a5c2bbdb8f45f0a2189d2834e5f60be3ead27
Ultrasty <1455670697@qq.com>
2020-05-14 21:15:30 +0800

    扫雷~

---
+++ 完整的c#项目/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public bool cai = true;
+        int n = 11;
+        Label[,] x=new Label[11,11];
+        bool[,] bomb = new bool[11, 11];
+        int[,] num = new int[13, 13];//指示周围雷的数量的数组
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int xWidth = SystemInformation.PrimaryMonitorSize.Width;
+            int yHeight = SystemInformation.PrimaryMonitorSize.Height; 
+            this.Location=new Point (0,yHeight/2-this.Height/2);
+            //int a,b;
+            //Random r = new Random();//产生雷的random对象
+            //int bombmax = 1;
+            
+            //while (bombmax <=10)//雷的总数22
+            //{
+            //    a = r.Next(0, n);
+            //    b = r.Next(0, n);
+            //    if (bomb[a, b] == false)
+            //    {
+            //        bombmax++;
+            //        bomb[a, b] = true;
+            //        for (int i = a; i <= a + 2; i++)
+            //        {
+            //            for (int j = b; j <= b + 2; j++)
+            //            {
+            //                num[i, j]++;
+            //            }
+            //        }
+            //    }
+            //}
+            //for (int i = 0; i < n; i++)
+            //{
+            //    for (int j = 0; j < n; j++)
+            //    {
+            //        x[i, j] = new Label();
+            //        x[i, j].Size = new Size(30, 30);
+            //        x[i, j].Location = new Point(20+32 * i, 20+32 * j);
+            //        x[i, j].AutoSize = false;
+            //        x[i, j].BorderStyle = BorderStyle.Fixed3D;
+            //        this.Controls.Add(x[i, j]);
+            //        x[i, j].Click += new EventHandler(this.lab_Click);
+            //        x[i, j].MouseDown += new MouseEventHandler(this.box_MouseDown);
+            //    }
+            //}
+        }
+        private void lab_Click(object sender, System.EventArgs e)
+        {
+
+            //int w = (((Label)sender).Left - 20) / 32;
+            //int h = (((Label)sender).Top - 20) / 32;
+            //if (bomb[w, h])
+            //    MessageBox.Show("你真菜");
+            //else
+            //{
+            //    ((Label)sender).Text = num[w + 1, h + 1].ToString();
+            //    ((Label)sender).Font = new Font("方正清刻本悦宋", 20);
+            //    ((Label)sender).ForeColor = Color.Blue;
+            //}
+
+            //for (int i = 0; i < n; i++)
+            //{
+            //    for (int j = 0; j < n; j++)
+            //    {
+            //        if (bomb[i, j]) x[i, j].BackColor = Color.Red;
+            //    }
+            //}
+        }
+        private void box_MouseDown(object sender, MouseEventArgs e)
+        {
+            int win=0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (x[i,j].Text != "" || x[i,j].BackColor != SystemColors.Control)
+                        win++;
+                }
+            }
+            if (win == n * n-1) MessageBox.Show("你真六","温馨提示");
+            if (e.Button == MouseButtons.Left)
+            {
+                int w = (((Label)sender).Left - 20) / 32;
+                int h = (((Label)sender).Top - 20) / 32;
+                if (bomb[w, h])
+                {
+                    
+                    for (int i = 0; i < n; i++)
+                    {
+                        for (int j = 0; j < n; j++)
+                        {
+                            if (bomb[i, j]) x[i, j].BackColor = Color.Red;
+                        }
+                    }
+                    Form2 frm2 = new Form2();
+                    while (cai)
+                    { 
+                        frm2.ShowDialog(this);
+                    }
+                }
+                else
+                {
+                    ((Label)sender).Text = num[w + 1, h + 1].ToString();
+                    ((Label)sender).Font = new Font("方正清刻本悦宋", 20);
+                    ((Label)sender).ForeColor = Color.Blue;
+                }
+                for (int o = 1; o <= 18;o++ )
+                    for (int i = 0; i < n ; i++)
+                    {
+                        for (int j = 0; j < n ; j++)
+                        {
+                            if (x[i, j].Text == "0")
+                                for (int g = i - 1; g <= i + 1; g++)
+                                {
+                                    for (int q = j - 1; q <= j + 1; q++)
+                                    {
+                                        try
+                                        {
+                                            x[g, q].Text = num[g + 1, q + 1].ToString();
+                                        }
+                                        catch
+                                        {
+                                            
+                                        }
+                                    }
+                                }
+
+                        }
+                    }
+                //try
+                //{
+                //    if (int.Parse(x[1, 1].Text) == 0 || int.Parse(x[0, 1].Text) == 0 || int.Parse(x[1, 0].Text) == 0)
+                //    {
+                //        x[0, 0].Text = "0";
+                //        x[0, 0].Font = new Font("方正清刻本悦宋", 20);
+                //        x[0, 0].ForeColor = Color.Blue;
+                //    }
+                    
+                
+                
+                
+                //}
+                //catch
+                //{ }
+                //try
+                //{
+                //    for (int s = 0; s <= 9; s++) { if (x[0, s].Text != "") { if (int.Parse(x[0, s].Text) == 0) x[0, s + 1].Text = num[0 + 1, s + 1 + 1].ToString(); } };//外围也连成一片
+                //    for (int s = 0; s <= 9; s++) if (x[s, 10].Text!=""){if (int.Parse(x[s, 10].Text) == 0) x[s + 1, 10].Text = num[s + 1 + 1, 10 + 1].ToString();};
+                //    for (int s = 9; s >= 1; s--) if (x[10, s].Text!=""){if (int.Parse(x[10, s].Text) == 0) x[10, s + 1].Text = num[10 + 1, s + 1 + 1].ToString();};
+                //    for (int s = 9; s >= 1; s--) if (x[s, 0].Text!=""){if (int.Parse(x[s, 0].Text) == 0) x[s + 1, 0].Text = num[s + 1 + 1, 0 + 1].ToString();};
+
+                //    for (int s = 0; s <= 9; s++) if (x[s, 0].Text!=""){if (int.Parse(x[s, 0].Text) == 0) x[s + 1, 0].Text = num[s + 1 + 1, 0 + 1].ToString();};
+                //    for (int s = 0; s <= 9; s++) if (x[10, s].Text!=""){if (int.Parse(x[10, s].Text) == 0) x[10, s + 1].Text = num[10 + 1, s + 1 + 1].ToString();};
+                //    for (int s = 9; s >= 1; s--) if (x[s, 10].Text!=""){if (int.Parse(x[s, 10].Text) == 0) x[s + 1, 10].Text = num[s + 1 + 1, 10 + 1].ToString();};
+                //    for (int s = 9; s >= 1; s--) if (x[0, s].Text != "") { if (int.Parse(x[0, s].Text) == 0) x[0, s + 1].Text = num[0 + 1, s + 1 + 1].ToString(); };
+                //}
+                //catch { }
+
+                
+                
+                 
+
+
+                    for (int i = 0; i < n; i++)
+                    {
+                        for (int j = 0; j < n; j++)
+                        {
+                            if (x[i, j].Text != "")
+                            {
+                                x[i, j].Font = new Font("方正清刻本悦宋", 20);
+                                x[i, j].ForeColor = Color.Blue;
+                            }
+                        }
+                    }
+            }
+            
+            
+            if (e.Button == MouseButtons.Right)
+            {
+             if (((Label)sender).BackColor == SystemColors.Control)
+                {
+                    int w = (((Label)sender).Left - 20) / 32;
+                    int h = (((Label)sender).Top - 20) / 32;
+                    x[w, h].BackColor = Color.Green;
+                }
+                else
+                {
+                    ((Label)sender).BackColor =SystemColors.Control;
+                }
+            }
+        }
+        int y;
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            cai = true;
+            try 
+            {
+                int d = int.Parse(textBox1.Text);
+            }
+            catch
+            {
+                return;
+            }
+            if (int.Parse(textBox1.Text) > 121 || int.Parse(textBox1.Text) < 1) return;
+            if (y == 0)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        x[i, j] = new Label();
+                        x[i, j].Size = new Size(30, 30);
+                        x[i, j].Location = new Point(20 + 32 * i, 20 + 32 * j);//把label放在合适的位置上
+                        x[i, j].AutoSize = false;
+                        x[i, j].BorderStyle = BorderStyle.Fixed3D;
+                        this.Controls.Add(x[i, j]);
+                        x[i, j].Click += new EventHandler(this.lab_Click);
+                        x[i, j].MouseDown += new MouseEventHandler(this.box_MouseDown);
+                    }
+                }
+                y = 1;
+            }
+            
+            for (int i = 0; i < n; i++)                        //初始化
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    bomb[i, j] = false;
+                    x[i, j].Text = "";
+                    x[i, j].BackColor = SystemColors.Control;
+                }
+            }
+            for (int i = 0; i < 13; i++)                       
+            {
+                for (int j = 0; j < 13; j++)
+                {
+
+                    num[i, j] = 0;
+                }
+            }
+            
+            
+            
+            int a, b;
+            Random r = new Random();//产生雷的random对象
+            int bombmax = 1;
+
+            while (bombmax <= int.Parse(textBox1.Text))//获取设定的雷的总数
+            {
+                a = r.Next(0, n);
+                b = r.Next(0, n);
+                if (bomb[a, b] == false)
+                {
+                    bombmax++;
+                    bomb[a, b] = true;
+                    for (int i = a; i <= a + 2; i++)
+                    {
+                        for (int j = b; j <= b + 2; j++)
+                        {
+                            num[i, j]++;
+                        }
+                    }
+                }
+            }
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cai = true;
+            for (int i = 0; i < n; i++)                        //初始化
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    
+                    x[i, j].Text = "";
+                    x[i, j].BackColor = SystemColors.Control;
+                }
+            }
+        }
+    }
+}
---
+++ 完整的c#项目/WindowsFormsApplication1/Form2.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("你真菜", "温馨提示");
+            this.Close();
+            Form1 frm1 = (Form1)this.Owner;
+            frm1.cai = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            int x = r.Next(0, this.Width - button2.Width - 10);
+            int y = r.Next(0, this.Height - button2.Height - 10);
+            button2.Location = new Point(x, y);
+        }
+
+        private void button2_MouseEnter(object sender, EventArgs e)
+        {
+            
+            
+            
+        }
+        
+        private void Form2_MouseMove(object sender, MouseEventArgs e)
+        {
+           
+            int a = button2.Left;
+            int b = button2.Left+button2.Width;
+            int c = button2.Top;
+            int d = button2.Top + button2.Height;
+
+
+
+            if (e.X < a && e.X > a - 20 && e.Y < d && e.Y > c)
+            {
+                button2.Left = e.X + 20;
+                if (b + 14 > this.Width)
+                    move();
+            }
+            if (e.X > b && e.X < b + 20 && e.Y < d && e.Y > c)
+            {
+                button2.Left = e.X - 20 -button2.Width;
+                if (a < 0)
+                    move();
+            }
+            if (e.X > a && e.X < b  && e.Y < d+20 && e.Y > d)
+            {
+                button2.Top  = e.Y - 20 - button2.Height;
+                if (c < 0)
+                    move();
+            }
+            if (e.X > a && e.X < b && e.Y < c && e.Y > c-20)
+            {
+                button2.Top = e.Y + 20 ;
+                if (d+37>this.Height)
+                    move();
+            }
+
+
+                //if (e.X > button2.Left+button2.Width)
+                //{
+                //    button2.Left = e.X - 12-button2.Width;
+                //    if (button2.Left< 0)
+                //        move();
+                //}
+                //if (e.Y < button2.Top)
+                //{
+                //    button2.Top = e.Y + 12;
+                //    if (button2.Top + button2.Height - 20 > this.Height)
+                //        move();
+                //}
+                //if (e.Y > button2.Top+button2.Height)
+                //{
+                //    button2.Top = e.Y - 12-button2.Height;
+                //    if (button2.Top<0)
+                //        move();
+                //}
+
+                
+
+            
+        }
+
+        private void button2_MouseMove(object sender, MouseEventArgs e)
+        {
+            move();
+        }
+
+        private void Form2_MouseHover(object sender, EventArgs e)
+        {
+            
+        }
+        private void move()
+        {
+        re: Random r = new Random();
+            int x = r.Next(0, this.Width - button2.Width - 20);
+            int y = r.Next(0, this.Height - button2.Height - 20);
+            if (x >= button2.Left && x <= button2.Left + button2.Width && y >= button2.Top && y <= button2.Top + button2.Height)
+                goto re;
+            button2.Location = new Point(x, y);
+            
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {                 
+            Form1 frm1 = (Form1)this.Owner;
+
+            int xWidth = SystemInformation.PrimaryMonitorSize.Width;
+            int yHeight = SystemInformation.PrimaryMonitorSize.Height;
+            this.Location = new Point(frm1.Width,yHeight/2-this.Height/2);
+
+            
+
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            int a = button2.Left;
+            int b = button2.Left + button2.Width;
+            int c = button2.Top;
+            int d = button2.Top + button2.Height;
+
+
+
+            if (e.X+pictureBox1.Left < a && e.X+pictureBox1.Left > a - 20 && e.Y+pictureBox1.Top < d && e.Y+pictureBox1.Top > c)
+            {
+                button2.Left = e.X+pictureBox1.Left + 20;
+                if (b + 14 > this.Width)
+                    move();
+            }
+            if (e.X+pictureBox1.Left > b && e.X+pictureBox1.Left < b + 20 && e.Y+pictureBox1.Top < d && e.Y+pictureBox1.Top > c)
+            {
+                button2.Left = e.X+pictureBox1.Left - 20 - button2.Width;
+                if (a < 0)
+                    move();
+            }
+            if (e.X+pictureBox1.Left > a && e.X+pictureBox1.Left < b && e.Y+pictureBox1.Top < d + 20 && e.Y+pictureBox1.Top > d)
+            {
+                button2.Top = e.Y+pictureBox1.Top - 20 - button2.Height;
+                if (c < 0)
+                    move();
+            }
+            if (e.X+pictureBox1.Left > a && e.X+pictureBox1.Left < b && e.Y+pictureBox1.Top < c && e.Y+pictureBox1.Top > c - 20)
+            {
+                button2.Top = e.Y+pictureBox1.Top + 20;
+                if (d + 37 > this.Height)
+                    move();
+            }
+        }
+
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+            int a = button2.Left;
+            int b = button2.Left + button2.Width;
+            int c = button2.Top;
+            int d = button2.Top + button2.Height;
+
+
+
+            if (e.X+button1.Left < a && e.X+button1.Left > a - 20 && e.Y+button1.Top < d && e.Y+button1.Top > c)
+            {
+                button2.Left = e.X+button1.Left + 20;
+                if (b + 14 > this.Width)
+                    move();
+            }
+            if (e.X+button1.Left > b && e.X+button1.Left < b + 20 && e.Y+button1.Top < d && e.Y+button1.Top > c)
+            {
+                button2.Left = e.X+button1.Left - 20 - button2.Width;
+                if (a < 0)
+                    move();
+            }
+            if (e.X+button1.Left > a && e.X+button1.Left < b && e.Y+button1.Top < d + 20 && e.Y+button1.Top > d)
+            {
+                button2.Top = e.Y+button1.Top - 20 - button2.Height;
+                if (c < 0)
+                    move();
+            }
+            if (e.X+button1.Left > a && e.X+button1.Left < b && e.Y+button1.Top < c && e.Y+button1.Top > c - 20)
+            {
+                button2.Top = e.Y+button1.Top + 20;
+                if (d + 37 > this.Height)
+                    move();
+            }
+        }
+    }
+}
